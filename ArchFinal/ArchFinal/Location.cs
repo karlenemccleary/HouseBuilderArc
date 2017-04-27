62b31e86d81748682458c6df2d16744de41785f5
KAYTLYNN CHUZIE <KNC5206@PSU-Erie.BD.Psu.Edu>
2017-04-27 11:03:19 -0400

    added location classes

---
+++ ArchFinal/ArchFinal/Location.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchFinal
+{
+    class Location : LocationIF
+    {
+        int price;
+
+        public Location(int price) {
+            this.price = price;
+        }
+
+        public int getPrice() {
+            return price;
+        }
+    }
+}
---
+++ ArchFinal/ArchFinal/PrivateIsland.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchFinal
+{
+    class PrivateIsland : Location
+    {
+        public PrivateIsland(): base(1000000) {
+            
+        }
+    }
+}
